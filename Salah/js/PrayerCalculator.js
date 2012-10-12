246d92b43f45e549acc0edf16d645f0a6446150c
Mahir <mahirqbl@gmail.com>
2012-10-12 00:43:00 -0500

    Initial commit

---
+++ Salah/js/PrayerCalculator.js
@@ -0,0 +1,74 @@
+﻿var PrayerCalculator = (function () {
+    function PrayerCalculator(location, parameters) {
+        this.location = location;
+        this.parameters = parameters;
+    }
+    PrayerCalculator.Methods = {
+            ISNA: {
+            fajrAngle: -15,
+            maghribAngle: -0.833,
+            ishaAngle: -15
+        },
+            Karachi: {
+            fajrAngle: -18,
+            maghribAngle: -0.833,
+            ishaAngle: -18
+        }
+    };
+    PrayerCalculator.prototype.calculateTimes = function (date) {
+        var factor = Math.PI / 180;
+        var latRadians = this.location.latitude * factor;
+        var solDec = this.getSolarDeclination(date);
+        var solarNoon = this.getSolarNoon(date);
+        var times = {
+    };
+        times.fajr = getAngleTime(this.parameters.fajrAngle * factor, date, true);
+        times.sunrise = getAngleTime(-0.0145385927, date, true);
+        times.dhuhr = solarNoon;
+        times.asr = getAngleTime(Math.atan(1 / (1 + Math.tan(Math.abs(latRadians - solDec)))), date, false);
+        times.maghrib = getAngleTime(this.parameters.maghribAngle * factor, date, false);
+        times.isha = this.parameters.ishaAngle ? getAngleTime(this.parameters.ishaAngle * factor, date, false) : new Date(times.maghrib.getTime() + this.parameters.ishaOnsetTime * 60000);
+        return times;
+        function getAngleTime(angle, date, beforeNoon) {
+            var timeOffset = Math.acos((Math.sin(angle) - Math.sin(latRadians) * Math.sin(solDec)) / (Math.cos(latRadians) * Math.cos(solDec))) / (15 * factor);
+            return new Date(solarNoon.getTime() + (beforeNoon ? -1 : 1) * 3600000 * timeOffset);
+        }
+    };
+    PrayerCalculator.prototype.getSolarNoon = function (date) {
+        var hourFrac = 12 + this.getTimezone() - this.location.longitude / 15 - this.getEquationOfTime(date) / 60;
+        var minFrac = 60 * (hourFrac - Math.floor(hourFrac));
+        var secFrac = 60 * (minFrac - Math.floor(minFrac));
+        return new Date(date.getFullYear(), date.getMonth(), date.getDate(), Math.floor(hourFrac), Math.floor(minFrac), Math.floor(secFrac), 0);
+    };
+    PrayerCalculator.prototype.getJulianDay = function (date) {
+        var calendarMonth = date.getMonth() + 1;
+        var a = Math.floor((14 - calendarMonth) / 12);
+        var y = date.getFullYear() + 4800 - a;
+        var m = calendarMonth + 12 * a - 3;
+        return date.getDate() + Math.floor(0.2 * (153 * m + 2)) + 365 * y + Math.floor(0.25 * y) - Math.floor(0.01 * y) + Math.floor(0.0025 * y) - 32045;
+    };
+    PrayerCalculator.prototype.getSolarDeclination = function (date) {
+        var factor = Math.PI / 180;
+        var n = this.getJulianDay(date) - 2451545;
+        var L = (280.459 + 0.98564736 * n) % 360;
+        var g = factor * (357.529 + 0.98560028 * n);
+        var EL = factor * (L + 1.915 * Math.sin(g) + 0.02 * Math.sin(2 * g));
+        var oe = factor * (23.439 - 3.6e-7 * n);
+        return Math.asin(Math.sin(oe) * Math.sin(EL));
+    };
+    PrayerCalculator.prototype.getEquationOfTime = function (date) {
+        var factor = Math.PI / 180;
+        var w = 360 / 365.24;
+        var start = new Date(date.getFullYear(), 0, 1);
+        var daysPassed = (date.getTime() - start.getTime()) / 86400000;
+        var a = w * (daysPassed + 10);
+        var b = a + 1.914 * Math.sin(w * (daysPassed - 2) * factor);
+        var c = (a - Math.atan(Math.tan(b * factor) / Math.cos(23.44 * factor)) / factor) / 180;
+        return 720 * (c - Math.floor(c + 0.5));
+    };
+    PrayerCalculator.prototype.getTimezone = function () {
+        var minuteDiff = (new Date()).getTimezoneOffset();
+        return -1 * minuteDiff / 60;
+    };
+    return PrayerCalculator;
+})();
---
+++ Salah/js/default.js
@@ -0,0 +1,35 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+    
+    var app = WinJS.Application;
+    app.addEventListener("activated", activatedHandler, false);
+    app.start();
+
+    function activatedHandler(eventArgs) {
+        var activation = Windows.ApplicationModel.Activation;
+        console.log("App activation.");
+        if (eventArgs.detail.kind == activation.ActivationKind.launch) {
+            console.log("App launched. Last state: " + eventArgs.detail.previousExecutionState);
+            if (eventArgs.detail.previousExecutionState != activation.ApplicationExecutionState.running) {
+                WinJS.Utilities.ready(function () {
+                    // ensure DOM ready
+                    console.log("Attaching event handlers to controls");
+                    document.getElementById("addButton").addEventListener("click", addButtonHandler);
+                }, false);
+            }
+        }
+
+        // eventArgs.setPromise defers tearing down the loading screen until the supplied promise is fulfilled.
+        eventArgs.setPromise(WinJS.UI.processAll());
+    }
+
+    function addButtonHandler(clickEvent) {
+        console.log("Add button clicked.");
+    }
+
+    function addTimes() {
+        console.log("Adding times");
+    }
+})();
