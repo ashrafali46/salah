4e3e27b36d3d08840a82b1804d3c6d9118db4fce
Mahir <mahirqbl@gmail.com>
2012-11-23 21:27:15 -0500

    Worked on background control (ironing out kinks), and background selector

--- Salah/js/BackgroundControls.js
+++ Salah/js/BackgroundControls.js
@@ -17,21 +17,26 @@ var Background = (function () {
 var BackgroundControl = (function () {
     function BackgroundControl(element) {
         this.element = element;
+        this.element.winControl = this;
         WinJS.Utilities.addClass(element, "backgroundControl");
         this._transitioning = false;
 
         this._transitionSequence = [];
         this._currentBackground = null;
+
+        this._ELEMENT_ZINDEX = (element.currentStyle.zIndex == "auto") ? 0 : element.currentStyle.zIndex;
     }
 
     BackgroundControl.prototype.set = function (id, imageURL) {
         var that = this;
 
         // check if this control contains that image already
-        var imageElement = this.element.querySelector("#" + id);
-        if (!imageElement) {
-            imageElement = document.createElement("img");
-            imageElement.id = id;
+        var bgElement = this.element.querySelector("#" + id);
+        if (!bgElement) {
+            bgElement = document.createElement("div");
+            bgElement.id = id;
+            var imageElement = document.createElement("img");
+            bgElement.appendChild(imageElement);
 
             imageElement.addEventListener("load", function () {
                 var shrinkAmount = 0.98 * Math.max(imageElement.naturalHeight / that.element.clientHeight, imageElement.naturalWidth / that.element.clientWidth);
@@ -46,22 +51,22 @@ var BackgroundControl = (function () {
             imageElement.addEventListener("dragstart", function (event) { event.preventDefault(); return false; }, true);
 
             imageElement.src = imageURL;
-            this.element.appendChild(imageElement);
+            this.element.appendChild(bgElement);
         }
 
-        return this._queueTransition(imageElement);
+        return this._queueTransition(bgElement);
     }
 
     // Returns a promise that is completed when the background has transitioned to the imageElement
-    BackgroundControl.prototype._queueTransition = function (imageElement) {
-        imageElement.style.opacity = 0;
+    BackgroundControl.prototype._queueTransition = function (bgElement) {
+        bgElement.style.opacity = 0;
 
         var callback;
         var transitionCompletePromise = new WinJS.Promise(function (complete) {
             callback = complete;
         });
 
-        this._transitionSequence.push({ image: imageElement, promiseCallback: callback });
+        this._transitionSequence.push({ element: bgElement, promiseCallback: callback });
         this._checkTransitionSequence();
 
         return transitionCompletePromise;
@@ -77,28 +82,40 @@ var BackgroundControl = (function () {
         this._transitioning = true;
 
         var nextBackgroundTransition = this._transitionSequence.shift();
-        var imageToTransition = nextBackgroundTransition.image;
+        var transitionElement = nextBackgroundTransition.element;
+        var transitionImage = transitionElement.firstElementChild;
 
         var imageLoadedPromise;
-        if (imageToTransition.complete) {
+        if (transitionImage.complete) {
             imageLoadedPromise = WinJS.Promise.wrap(null);
         } else {
             imageLoadedPromise = new WinJS.Promise(function (complete) {
-                imageToTransition.addEventListener("load", function () {
+                transitionImage.addEventListener("load", function () {
                     setTimeout(complete, 40) // try to prevent pop in
                 });
             });
         }
 
         imageLoadedPromise.then(function () {
-            var lastCurrent = (that._currentBackground) ? that.element.querySelector("#" + that._currentBackground) : null;
-            if (lastCurrent) {
-                WinJS.Utilities.removeClass(lastCurrent, "current");
-                WinJS.Utilities.addClass(lastCurrent, "lastCurrent");
+            // set a new lastCurrent, clearing the oldLastCurrent
+            var oldLastCurrent = that.element.querySelector(".lastCurrent");
+            if (oldLastCurrent) {
+                WinJS.Utilities.removeClass(oldLastCurrent, "lastCurrent");
+                oldLastCurrent.style.zIndex = that._ELEMENT_ZINDEX - 2;
             }
-            that._currentBackground = imageToTransition.id;
-            WinJS.Utilities.addClass(imageToTransition, "current");
-            WinJS.UI.Animation.fadeIn(imageToTransition).then(function () {
+
+            if (that._currentBackground) {
+                var oldCurrent = that.element.querySelector("#" + that._currentBackground);
+                WinJS.Utilities.removeClass(oldCurrent, "current");
+                WinJS.Utilities.addClass(oldCurrent, "lastCurrent");
+                oldCurrent.style.zIndex = that._ELEMENT_ZINDEX - 1;
+            }
+
+            that._currentBackground = transitionElement.id;
+            WinJS.Utilities.addClass(transitionElement, "current");
+            transitionElement.style.zIndex = that._ELEMENT_ZINDEX;
+
+            WinJS.UI.Animation.fadeIn(transitionElement).then(function () {
                 that._transitioning = false;
                 nextBackgroundTransition.promiseCallback();
 
@@ -128,6 +145,8 @@ var BackgroundSelector = (function () {
         this.element = element;
         WinJS.Utilities.addClass(element, "backgroundSelector");
 
+        this.busy = false;
+        this.selectedChoice = null;
         this._choicesArray = [];
 
         // setup html
@@ -135,16 +154,19 @@ var BackgroundSelector = (function () {
     }
 
     BackgroundSelector.prototype._loadHtml = function () {
-        this._selected = document.createElement("div");
-        this._selected.className = "selected";
+        var selected = document.createElement("div");
+        selected.className = "selected";
+        var selectedBackgroundEl = document.createElement("div");
+        selected.appendChild(selectedBackgroundEl);
+        this._backgroundControl = new BackgroundControl(selectedBackgroundEl);
 
         this._choices = document.createElement("div");
         this._choices.className = "choicesList";
 
-        this.element.appendChild(this._selected);
+        this.element.appendChild(selected);
         this.element.appendChild(this._choices);
 
-        this._selected.style.height = (this._selected.offsetWidth * .5625) + "px";
+        selected.style.height = (selected.offsetWidth * .5625) + "px"; // 16:9 ratio of width to height
     }
 
     BackgroundSelector.prototype.addChoice = function (choice) {
@@ -153,33 +175,77 @@ var BackgroundSelector = (function () {
 
         this._choicesArray.push(choice);
 
-        var otherChoices = this._choices.querySelectorAll(".choice");
+        var otherChoices = this._choices.querySelectorAll(".choice:not([deleting])");
 
         var choiceEl = document.createElement("div");
+        choiceEl.id = choice.id;
         choiceEl.choice = choice;
         choiceEl.className = "choice";
-        choiceEl.style.height = choiceEl.offsetWidth + "px";
+        var size = (0.2 * this.element.offsetWidth - 12);
+        choiceEl.style.height = size + "px"; //choiceEl.offsetWidth + "px";
+        choiceEl.style.width = size + "px";
         choiceEl.style.backgroundImage = "url('" + choice.thumb + "')";
-        choiceEl.style.backgroundSize = choiceEl.offsetWidth + "px " + choiceEl.offsetWidth + "px";
+        choiceEl.style.backgroundSize = size + "px " + size + "px";
         var highlightDiv = document.createElement("div");
         highlightDiv.className = "highlight";
         choiceEl.appendChild(highlightDiv);
         var that = this;
         choiceEl.addEventListener("click", function () { that.selectChoice(choice); });
+        choiceEl.addEventListener("MSPointerDown", function () { WinJS.UI.Animation.pointerDown(choiceEl); });
+        choiceEl.addEventListener("MSPointerOut", function () { WinJS.UI.Animation.pointerUp(choiceEl); });
 
         var addToList = WinJS.UI.Animation.createAddToListAnimation(choiceEl, otherChoices);
-        this._choices.appendChild(choiceEl);
-        addToList.execute();
+        this._choices.insertBefore(choiceEl, this._choices.firstElementChild);
+        return addToList.execute();
     }
 
     BackgroundSelector.prototype.removeChoice = function (choice) {
+        var choiceIndex = this._choicesArray.indexOf(choice);
+        if (choiceIndex == -1)
+            return;
+
+        this._choicesArray.splice(choiceIndex, 1);
+        var choiceEl = this._choices.querySelector("#" + choice.id);
+
+        if (!choiceEl || choiceEl.deleting)
+            return;
+
         choiceEl.setAttribute("deleting", true);
-        //WinJS.UI.Animation.createDeleteFromListAnimation(choiceEl, that._choicesArray
-        //WinJS.UI.Animation.deleteFromList(
+        var affectedChoices = this._choices.querySelectorAll("div:not([deleting])");
+        var deleteAnim = WinJS.UI.Animation.createDeleteFromListAnimation(choiceEl, affectedChoices);
+        choiceEl.style.position = "fixed";
+        choiceEl.style.zIndex = -1;
+        choiceEl.style.opacity = 0;
+
+        return deleteAnim.execute().then(function () {
+            choiceEl.parentNode && choiceEl.parentNode.removeChild(choiceEl);
+        });
     }
 
     BackgroundSelector.prototype.selectChoice = function (choice) {
+        if (this.busy)
+            return false;
 
+        this.busy = true;
+
+        var busyPromises = [];
+        this._backgroundControl.set(choice.id, choice.imageURL);
+
+        busyPromises.push(this.removeChoice(choice));
+        if (this.selectedChoice) {
+            var deletedChoice = this._choices.querySelector("[deleting]");
+            deletedChoice.style.marginLeft = (-1 * (deletedChoice.offsetWidth + parseFloat(deletedChoice.currentStyle.marginRight))) + 'px';
+            busyPromises.push(this.addChoice(this.selectedChoice));
+        }
+
+        this.selectedChoice = choice;
+
+        var that = this;
+        WinJS.Promise.join(busyPromises).then(function () {
+            that.busy = false;
+        });
+
+        return true;
     }
 
     return BackgroundSelector;
